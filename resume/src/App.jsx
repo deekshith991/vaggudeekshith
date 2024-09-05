a90dcb4b5fec3c45eef9fb408197f8de8c56ec34
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-09-05 14:27:13 +0530

    Update Vite: basic site done

---
+++ resume/src/App.jsx
@@ -0,0 +1,12 @@
+import { Box ,Text} from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <Box fontStyle={"JetBrains Nerd Mono"} minH={"100vh"} backgroundColor={"#fff"}>
+            <Text fontSize='2xl' as='b'>Vaggu Deekshith</Text>
+    </Box>
+  )
+}
+
+export default App
